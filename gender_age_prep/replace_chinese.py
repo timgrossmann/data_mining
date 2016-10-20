4ecdba321ccce51a8d912c63d3a1430fff071a92
timgrossmann <t.grossmann1994@web.de>
2016-10-20 18:13:48 +0200

    started chinese conversion

--- gender_age_prep/replace_chinese.py
+++ gender_age_prep/replace_chinese.py
@@ -1,1 +1,9 @@
-# -*- coding: utf-8 -*-
+import pandas as pd
+
+chin_eng = pd.read_csv('./data/brandMap.txt', sep=' ', encoding='utf-8')
+
+def replace_pinyin(text):
+  for row in chin_eng.iterrows():
+    text = text.replace(row[0], row[1][0])
+
+  return text
