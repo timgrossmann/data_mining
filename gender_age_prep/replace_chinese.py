30f2c4e774efba811ec5204cbf7fb5967f59a162
timgrossmann <t.grossmann1994@web.de>
2016-10-20 18:55:55 +0200

    replace_chinese works now / the first arg is the file to convert

--- gender_age_prep/replace_chinese.py
+++ gender_age_prep/replace_chinese.py
@@ -1,4 +1,8 @@
+import sys
 import pandas as pd
+
+reload(sys)
+sys.setdefaultencoding('UTF8')
 
 chin_eng = pd.read_csv('./data/brandMap.txt', sep=' ', encoding='utf-8')
 
@@ -7,3 +11,13 @@ def replace_pinyin(text):
     text = text.replace(row[0], row[1][0])
 
   return text
+
+lines = []
+
+with open('./data/' + sys.argv[1], 'r') as file:
+  for line in file:
+    lines.append(line)
+
+with open('./data/' + sys.argv[1], 'w') as new_file:
+  for line in lines:
+    new_file.write(replace_pinyin(line.decode('utf-8')))
